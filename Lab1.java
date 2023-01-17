18c6bd3e2364c9c48895d3f6751df30fc9f074a3
preetisharmadal <preeti28062000@gmail.com>
2023-01-17 18:03:42 -0400

    Added new cases

--- Lab1.java
+++ Lab1.java
@@ -8,19 +8,21 @@
 // 6. Remove value using invalid keys (non-existent key,wrong datatype)
 // 7. Remove value using valid key
 
+
+
 public class Lab1 {
     public static void main(String[] args) {
 //        Instantiating map with <Integer,Integer> key-value pair
         Map mpIntInt = new Map(2, 3);
 //        1. Add key-value pair
         try {
-            mpIntInt.addPair(3, 6);
+            mpIntInt.put(3, 6);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 // 2. Add key-value pair with duplicate key
         try {
-            mpIntInt.addPair(3, 7);
+            mpIntInt.put(3, 7);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -28,17 +30,16 @@ public class Lab1 {
 
 // 4. Read value using valid key
         try {
-            System.out.println("key: 3, value: " + mpIntInt.getValue(3));
+            System.out.println("key: 3, value: " + mpIntInt.get(3));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
 // 5. Read value using invalid keys (non-existent key,wrong datatype)
         try {
-            System.out.println("key: 5, value: " + mpIntInt.getValue(5));
+            System.out.println("key: 5, value: " + mpIntInt.get(5));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-//        System.out.println("key: 3, value: " + mpIntInt.getValue(3));
 // 6. Remove value using invalid keys (non-existent key,wrong datatype)
         try {
             mpIntInt.removePair(6);
@@ -52,9 +53,103 @@ public class Lab1 {
             System.out.println(e.getMessage());
         }
         try {
-            mpIntInt.getValue(3);
+            mpIntInt.get(3);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+//        Instantiating map with <String,String> key-value pair
+        Map mpStringString = new Map("d", "default");
+//        1. Add key-value pair
+        try {
+            mpStringString.put("a", "apple");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 2. Add key-value pair with duplicate key
+        try {
+            mpStringString.put("a", "banana");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 3. Add key-value with wrong datatype
+
+// 4. Read value using valid key
+        try {
+            System.out.println("key: a, value: " + mpStringString.get("a"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 5. Read value using invalid keys (non-existent key,wrong datatype)
+        try {
+            System.out.println("key: c, value: " + mpStringString.get("c"));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 6. Remove value using invalid keys (non-existent key,wrong datatype)
+        try {
+            mpStringString.removePair("d");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 7. Remove value using valid key
+        try {
+            mpStringString.removePair("a");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            mpStringString.get("a");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+//        Instantiating map with <Double,Double> key-value pair
+        Map mpDoubleDouble = new Map(1.1, 3.3);
+//        1. Add key-value pair
+        try {
+            mpDoubleDouble.put(3.3, 6.6);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 2. Add key-value pair with duplicate key
+        try {
+            mpDoubleDouble.put(3.3, 7.7);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 3. Add key-value with wrong datatype
+
+// 4. Read value using valid key
+        try {
+            System.out.println("key: 3.3, value: " + mpDoubleDouble.get(3.3));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 5. Read value using invalid keys (non-existent key,wrong datatype)
+        try {
+            System.out.println("key: 5.5, value: " + mpIntInt.get(5.5));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+//        System.out.println("key: 3.3, value: " + mpIntInt.get(3.3));
+// 6. Remove value using invalid keys (non-existent key,wrong datatype)
+        try {
+            mpDoubleDouble.removePair(6.6);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+// 7. Remove value using valid key
+        try {
+            mpDoubleDouble.removePair(3.3);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            mpDoubleDouble.get(3.3);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
--- Map.java
+++ Map.java
@@ -13,17 +13,30 @@ public class Map {
     private ArrayList<Double> doubleKeys = new ArrayList<Double>();
     private ArrayList<Double> doubleValues = new ArrayList<Double>();
 
-        
+    enum DataTypes{
+        intInt, 
+        stringString,
+        doubleDouble,
+        intString,
+        intDouble,
+        stringInt,
+        stringDouble,
+        doubleInt,
+        doubleString
+    }    
+
+    DataTypes dataType;
 
     //    Constructor to accept <Integer, Integer>
     public Map(int key, int value) {
+        dataType = DataTypes.intInt;
         intKeys.add(key);
         intValues.add(value);
     }
 
     //    Constructor to accept <String, String>
-
     public Map(String key, String value) {
+        dataType = DataTypes.stringString;
         stringKeys.add(key);
         stringValues.add(value);
     }
@@ -31,12 +44,59 @@ public class Map {
     //    Constructor to accept <Double, Double>
 
     public Map(double key, double value) {
+        dataType = DataTypes.doubleDouble;
         doubleKeys.add(key);
         doubleValues.add(value);
-    }    
+    }   
+    
+    //    Constructor to accept <Integer, String>
+    public Map(int key, String value) {
+        dataType = DataTypes.intString;
+        intKeys.add(key);
+        stringValues.add(value);
+    }  
+
+    //    Constructor to accept <Integer, Double>
+    public Map(int key, double value) {
+        dataType = DataTypes.intDouble;
+        intKeys.add(key);
+        doubleValues.add(value);
+    }  
+
+    //    Constructor to accept <String, Integer>
+
+    public Map(String key, int value) {
+        dataType = DataTypes.stringInt;
+        stringKeys.add(key);
+        intValues.add(value);
+    }
+
+    //    Constructor to accept <String, Double>
+
+    public Map(String key, double value) {
+        dataType = DataTypes.stringDouble;
+        stringKeys.add(key);
+        doubleValues.add(value);
+    }
+
+    //    Constructor to accept <Double, Integer>
+
+    public Map(double key, int value) {
+        dataType = DataTypes.doubleInt;
+        doubleKeys.add(key);
+        intValues.add(value);
+    }
+
+    //    Constructor to accept <Double, String>
+
+    public Map(double key, String value) {
+        dataType = DataTypes.doubleString;
+        doubleKeys.add(key);
+        stringValues.add(value);
+    }
 
     //    add key-value for <Integer, Integer>
-    public void addPair(int key, int value) throws Exception {
+    public void put(int key, int value) throws Exception {
 //        Adding pair with unique key validation
         if (intKeys.contains(key)) {
             throw new Exception("Cannot add: Duplicate key exists");
@@ -48,7 +108,7 @@ public class Map {
     }
 
     //    add key-value for <String, String>
-    public void addPair(String key, String value) throws Exception {
+    public void put(String key, String value) throws Exception {
         //        Adding pair with unique key validation
                 if (stringKeys.contains(key)) {
                     throw new Exception("Cannot add: Duplicate key exists");
@@ -59,7 +119,7 @@ public class Map {
         
             }
     //    add key-value for <Double, Double>
-    public void addPair(double key, double value) throws Exception {
+    public void put(double key, double value) throws Exception {
         //        Adding pair with unique key validation
                 if (doubleKeys.contains(key)) {
                     throw new Exception("Cannot add: Duplicate key exists");
@@ -70,9 +130,81 @@ public class Map {
         
             }
 
+    //    add key-value for <Integer, String>
+    public void put(int key, String value) throws Exception {
+        //        Adding pair with unique key validation
+                if (intKeys.contains(key)) {
+                    throw new Exception("Cannot add: Duplicate key exists");
+                } else {
+                    intKeys.add(key);
+                    stringValues.add(value);
+                }
+        
+            }
+
+    //    add key-value for <Integer, Double>
+    public void put(int key, double value) throws Exception {
+        //        Adding pair with unique key validation
+                if (intKeys.contains(key)) {
+                    throw new Exception("Cannot add: Duplicate key exists");
+                } else {
+                    intKeys.add(key);
+                    doubleValues.add(value);
+                }
+        
+            }
+
+    //    add key-value for <String, Integer>
+    public void put(String key, int value) throws Exception {
+        //        Adding pair with unique key validation
+                if (stringKeys.contains(key)) {
+                    throw new Exception("Cannot add: Duplicate key exists");
+                } else {
+                    stringKeys.add(key);
+                    intValues.add(value);
+                }
+        
+            }
+
+    //    add key-value for <String, Double>
+    public void put(String key, double value) throws Exception {
+        //        Adding pair with unique key validation
+                if (stringKeys.contains(key)) {
+                    throw new Exception("Cannot add: Duplicate key exists");
+                } else {
+                    stringKeys.add(key);
+                    doubleValues.add(value);
+                }
+        
+            }
+
+    //    add key-value for <Double, Integer>
+    public void put(double key, int value) throws Exception {
+        //        Adding pair with unique key validation
+                if (doubleKeys.contains(key)) {
+                    throw new Exception("Cannot add: Duplicate key exists");
+                } else {
+                    doubleKeys.add(key);
+                    intValues.add(value);
+                }
+        
+            }
+
+    //    add key-value for <Double, String>
+    public void put(double key, String value) throws Exception {
+        //        Adding pair with unique key validation
+                if (doubleKeys.contains(key)) {
+                    throw new Exception("Cannot add: Duplicate key exists");
+                } else {
+                    doubleKeys.add(key);
+                    stringValues.add(value);
+                }
+        
+            }
+
 
     //    Get value from key for <Integer,Integer>
-    public int getValue(int key) throws Exception {
+    public int get(int key) throws Exception {
 //        Fetching value with key not found validation
         if (intKeys.contains(key)) {
             int index = intKeys.indexOf(key);
@@ -84,7 +216,7 @@ public class Map {
     }
 
     //    Get value from key for <String,String>
-    public String getValue(String key) throws Exception {
+    public String get(String key) throws Exception {
         //        Fetching value with key not found validation
                 if (stringKeys.contains(key)) {
                     int index = stringKeys.indexOf(key);
@@ -96,7 +228,32 @@ public class Map {
             }
 
     //    Get value from key for <Double,Double>
-    public double getValue(double key) throws Exception {
+    public double get(double key) throws Exception {
+        //        Fetching value with key not found validation
+                if (doubleKeys.contains(key)) {
+                    int index = doubleKeys.indexOf(key);
+                    double value = doubleValues.get(index);
+                    return value;
+                } else {
+                    throw new Exception("Cannot get value: Key does not exist");
+                }
+            }
+
+    //    Get value from key for <Integer,String>
+
+    public String get(int key) throws Exception {
+        //        Fetching value with key not found validation
+                if (intKeys.contains(key)) {
+                    int index = intKeys.indexOf(key);
+                    String value = stringValues.get(index);
+                    return value;
+                } else {
+                    throw new Exception("Cannot get value: Key does not exist");
+                }
+            }
+
+    //    Get value from key for <Integer,Double>
+    public double get(int key) throws Exception {
         //        Fetching value with key not found validation
                 if (doubleKeys.contains(key)) {
                     int index = doubleKeys.indexOf(key);
@@ -139,7 +296,7 @@ public class Map {
     public void removePair(double key) throws Exception {
         //        Deleting pair with key not found validation
                 if (doubleKeys.contains(key)) {
-                    double index = doubleKeys.indexOf(key);
+                    int index = doubleKeys.indexOf(key);
                     doubleKeys.remove(index);
                     doubleValues.remove(index);
         
@@ -149,4 +306,32 @@ public class Map {
         
             }            
             
+    //    Remove key-value for <Integer,String>
+    public void removePair(int key) throws Exception {
+        //        Deleting pair with key not found validation
+                if (intKeys.contains(key)) {
+                    int index = doubleKeys.indexOf(key);
+                    intKeys.remove(index);
+                    stringValues.remove(index);
+        
+                } else {
+                    throw new Exception("Cannot remove pair: Key does not exist");
+                }
+        
+            } 
+
+    //    Remove key-value for <Integer,Double>
+    public void removePair(double key) throws Exception {
+        //        Deleting pair with key not found validation
+                if (doubleKeys.contains(key)) {
+                    double index = doubleKeys.indexOf(key);
+                    doubleKeys.remove(index);
+                    doubleValues.remove(index);
+        
+                } else {
+                    throw new Exception("Cannot remove pair: Key does not exist");
+                }
+        
+            } 
+
 }
